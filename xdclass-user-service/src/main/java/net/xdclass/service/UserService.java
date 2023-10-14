cbf96f7f338b80b946e737646edfa0f5665bdbf5
kevin jia <kevinjia1984@gmail.com>
2023-10-14 19:08:31 +1100

    init checkin

---
+++ xdclass-user-service/src/main/java/net/xdclass/service/UserService.java
@@ -0,0 +1,16 @@
+package net.xdclass.service;
+
+import net.xdclass.model.UserDO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 二当家小D
+ * @since 2023-10-14
+ */
+public interface UserService extends IService<UserDO> {
+
+}
---
+++ xdclass-user-service/src/main/java/net/xdclass/service/impl/AddressServiceImpl.java
@@ -0,0 +1,20 @@
+package net.xdclass.service.impl;
+
+import net.xdclass.model.AddressDO;
+import net.xdclass.mapper.AddressMapper;
+import net.xdclass.service.AddressService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 电商-公司收发货地址表 服务实现类
+ * </p>
+ *
+ * @author 二当家小D
+ * @since 2023-10-14
+ */
+@Service
+public class AddressServiceImpl extends ServiceImpl<AddressMapper, AddressDO> implements AddressService {
+
+}
